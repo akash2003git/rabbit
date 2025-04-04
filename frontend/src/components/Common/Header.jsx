b6df4ff149aeaf63cf740d43feef2a424f69745b
akash2003git <akash.tayade.dev@gmail.com>
2025-04-04 23:55:42 +0530

    Initial Commit

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,14 @@
+import Topbar from "../Layout/Topbar";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <Topbar />
+      <Navbar />
+      {/* Cart Drawer */}
+    </header>
+  );
+};
+
+export default Header;
