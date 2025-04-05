d075552709af18b2dfd98b9f59f8a1fe75947518
akash2003git <akash.tayade.dev@gmail.com>
2025-04-05 18:00:33 +0530

    Finished Header frontend

---
+++ frontend/src/components/Cart/CartContents.jsx
@@ -0,0 +1,59 @@
+import { RiDeleteBin3Line } from "react-icons/ri";
+
+const CartContents = () => {
+  const cartProducts = [
+    {
+      productId: 1,
+      name: "T-shirt",
+      size: "M",
+      color: "Red",
+      quantity: 1,
+      price: 15,
+      image: "https://picsum.photos/200?random=1",
+    },
+    {
+      productId: 2,
+      name: "Jeans",
+      size: "L",
+      color: "Blue",
+      quantity: 1,
+      price: 25,
+      image: "https://picsum.photos/200?random=2",
+    },
+  ];
+
+  return cartProducts.map((product, index) => (
+    <div key={index} className="flex items-start justify-between py-4 border-b">
+      <div className="flex items-start">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-20 h-24 object-cover mr-4 rounded"
+        />
+        <div>
+          <h3>{product.name}</h3>
+          <p className="text-sm text-gray-500">
+            size: {product.size} | color: {product.color}
+          </p>
+          <div className="flex items-center mt-2">
+            <button className="border rounded w-8 h-8 text-xl font-medium cursor-pointer">
+              -
+            </button>
+            <span className="mx-4">{product.quantity}</span>
+            <button className="border rounded w-8 h-8 text-xl font-medium cursor-pointer">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>$ {product.price.toLocaleString()}</p>
+        <button className="cursor-pointer">
+          <RiDeleteBin3Line className="h-6 w-6 mt-2 text-red-600" />
+        </button>
+      </div>
+    </div>
+  ));
+};
+
+export default CartContents;
