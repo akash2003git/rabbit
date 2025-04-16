480ac51957e752c726569b3f8513a61031eee198
akash2003git <akash.tayade.dev@gmail.com>
2025-04-16 23:49:59 +0530

    Product Schema

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const Product = require("../models/Product");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+// @route POST /api/products
+// @desc Create a new Product
+// @access Private/Admin
